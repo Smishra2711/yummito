48efa7a14a6b3f3bdf3d670f65d38bb224be335b
Shubham Mishra <mishrashubham133@gmail.com>
2021-09-11 22:54:00 +0530

    stores

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MasterService } from "../../Service/master.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-
   cat = [];
-  constructor() { }
+  stores = [];
+  lat = "9.8716317";
+  lon = "76.4926357";
+  constructor(private service: MasterService) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  showStore() {}
+
+  getAllStore(type) {
+    let apiPath =
+      "vendor/store-list?lat=9.8716317&lon=76.4926357&store_type=" + type + "";
+    this.service.get(apiPath).subscribe(
+      (res) => {
+        this.stores.push(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
