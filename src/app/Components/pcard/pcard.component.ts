13f6b08e5bf8b6fdc146840678ae289811db9f48
Shubham Mishra <mishrashubham133@gmail.com>
2021-09-11 23:26:12 +0530

    stores

--- src/app/Components/pcard/pcard.component.ts
+++ src/app/Components/pcard/pcard.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-pcard',
-  templateUrl: './pcard.component.html',
-  styleUrls: ['./pcard.component.css']
+  selector: "app-pcard",
+  templateUrl: "./pcard.component.html",
+  styleUrls: ["./pcard.component.css"]
 })
 export class PcardComponent implements OnInit {
+  @Input()
+  name: string;
+  subStoreType: string;
+  rateForTwo: string;
+  cookingTime: string;
+  distance: string;
+  img: string;
+  logo: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/Pages/resturant/resturant.component.ts
+++ src/app/Pages/resturant/resturant.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MasterService } from "src/app/Service/master.service";
 
 @Component({
-  selector: 'app-resturant',
-  templateUrl: './resturant.component.html',
-  styleUrls: ['./resturant.component.css']
+  selector: "app-resturant",
+  templateUrl: "./resturant.component.html",
+  styleUrls: ["./resturant.component.css"]
 })
 export class ResturantComponent implements OnInit {
+  constructor(private service: MasterService) {}
 
-  constructor() { }
-
+  stores = [];
   ngOnInit(): void {
+    this.getAllStore("Resturant");
   }
 
+  getAllStore(type) {
+    let apiPath =
+      "vendor/store-list?lat=9.8716317&lon=76.4926357&store_type=Restaurant";
+    this.service.get(apiPath).subscribe(
+      (res) => {
+        this.stores.push(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
