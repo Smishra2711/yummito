1310a2dec61cc0de87284032530594f35c938d25
Shubham Mishra <mishrashubham133@gmail.com>
2021-09-11 22:58:23 +0530

    storePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,47 +1,50 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Pages/login/login.component';
-import { RegisterComponent } from './Pages/register/register.component';
-import { DashboardComponent } from './Pages/dashboard/dashboard.component';
-import { HomeComponent } from './Pages/home/home.component';
-import { ProfileComponent } from './Pages/profile/profile.component';
-import { CartComponent } from './Pages/cart/cart.component';
-import { AppComponent } from './app.component';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./Pages/login/login.component";
+import { RegisterComponent } from "./Pages/register/register.component";
+import { DashboardComponent } from "./Pages/dashboard/dashboard.component";
+import { HomeComponent } from "./Pages/home/home.component";
+import { ProfileComponent } from "./Pages/profile/profile.component";
+import { CartComponent } from "./Pages/cart/cart.component";
+import { AppComponent } from "./app.component";
+import { ResturantComponent } from "./Pages/resturant/resturant.component";
 
 const routes: Routes = [
   {
-    path: 'login',
-    component:LoginComponent
+    path: "login",
+    component: LoginComponent
   },
   {
-    path:'register',
-    component:RegisterComponent
+    path: "register",
+    component: RegisterComponent
   },
   {
-    path:'home',
-    component:HomeComponent
+    path: "home",
+    component: HomeComponent
   },
   {
-    path:'profile',
-    component:ProfileComponent
+    path: "profile",
+    component: ProfileComponent
   },
   {
-    path:'cart',
-    component:CartComponent
+    path: "cart",
+    component: CartComponent
   },
   {
-    path:'dashboard',
-    component:DashboardComponent
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "store/:type",
+    component: ResturantComponent
   }
-  
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
 
 export const routingComponents = [
   LoginComponent,
